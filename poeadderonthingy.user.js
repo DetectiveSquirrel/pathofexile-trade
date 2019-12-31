df0ce98c584acfeebc7c4db654504c569b4a21d1
Detective Squirrel <ratman7539@windowslive.com>
2019-12-31 14:14:34 +1300

    Update poeadderonthingy.user.js

--- poeadderonthingy.user.js
+++ poeadderonthingy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Always show Pathofexile.com/trade affix ranges
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/DetectiveSquirrel/pathofexile-trade
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.pathofexile.com/trade*
